088f37147cc4d38f760f0ac06471c7e09b9d4fbc
John Yang <byjohnyang@gmail.com>
2024-04-16 16:14:36 -0400

    Add template for generating leaderboard automatically

---
+++ template/generate_index.py
@@ -0,0 +1,19 @@
+import json
+from jinja2 import Environment, FileSystemLoader
+
+# Set up Jinja2 environment
+env = Environment(loader=FileSystemLoader('.'))
+
+# Load the template
+template = env.get_template('template.html')
+
+# Load JSON data
+with open('data.json', 'r') as f:
+    data = json.load(f)
+
+# Render the template with the data
+output_html = template.render(data)
+
+# Write the output to a new HTML file
+with open('../index.html', 'w') as f:
+    f.write(output_html)
