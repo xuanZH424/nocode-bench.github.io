6f5728afa04d9a7a5f588724c929e8f1c9c6e334
John Yang <byjohnyang@gmail.com>
2024-06-05 01:12:03 -0400

    Add clickable to main results

--- js/mainResults.js
+++ js/mainResults.js
@@ -12,6 +12,32 @@ const statusToNaturalLanguage = {
     'resolved': 'Resolved'
 }
 
+function updateLogViewer(inst_id, split, model) {
+    if (inst_id == 'No Instance Selected') {
+        const logViewer = document.querySelector('#log-viewer');
+        logViewer.innerHTML = 'No instance selected.';
+        return;
+    }
+    const url = `https://raw.githubusercontent.com/swe-bench/experiments/main/evaluation/${split}/${model}/logs/${inst_id}.${model}.eval.log`;
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            const logViewer = document.querySelector('#log-viewer');
+            logViewer.innerHTML = '';
+
+            const inst_p = document.createElement('p');
+            inst_p.textContent = `Instance ID: ${inst_id}`;
+            logViewer.appendChild(inst_p);
+
+            const pre = document.createElement('pre');
+            pre.textContent = data;
+            logViewer.appendChild(pre);
+        })
+        .catch(error => {
+            console.error('Error fetching the JSON data:', error);
+        });
+}
+
 function createTableHeader(keys, table) {
     const headerRowWrapper = document.createElement('thead');
     const headerRow = document.createElement('tr');
@@ -36,6 +62,12 @@ function createTableBody(keys, data, table) {
         ids.forEach(id => {
             const div = document.createElement('div');
             div.textContent = id;
+            if (!(status === 'no_generation' || status === 'generated')) {
+                div.classList.add('instance');
+                div.style.cursor = 'pointer';
+            } else {
+                div.classList.add('instance-not-clickable');
+            }
             td.appendChild(div);
         });
 
